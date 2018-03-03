d010b12dc93bd85097cf8f97725a2eb1fcffabcc
Davi Barreto <davivbarreto@gmail.com>
2018-03-03 14:15:00 +0200

    add temp hacky scraper files

---
+++ scrape imdb/database_model.py
@@ -0,0 +1,199 @@
+from peewee import *
+from sklearn.neighbors import KNeighborsClassifier
+from playhouse.shortcuts import model_to_dict
+from collections import Counter
+import datetime
+
+db = SqliteDatabase('movie_ratings.db')
+
+
+class User(Model):
+
+    id = PrimaryKeyField()
+    ip_address = CharField()
+    grades = TextField(default="1" * 1)
+    date_added = DateTimeField(default=datetime.datetime.now)
+    archived = BooleanField(default=False)
+
+    class Meta:
+        database = db
+
+
+class Movie(Model):
+
+    id = PrimaryKeyField()
+    name = CharField(default="asd")
+    grade = FloatField(default=1)
+    sinopse = TextField(default="asd")
+    imdb_link = TextField(default="asd")
+    poster_link = TextField(default="asd")
+    date_added = DateTimeField(default=datetime.datetime.now)
+    archived = BooleanField(default=False)
+
+    class Meta:
+        database = db
+
+
+class DBHandler():
+    ''' this class contains the utility functions for the model '''
+
+    def update_ratings_length():
+        ''' make sure len(User.grades) == number of movies in database '''
+        number_of_movies = len(DBHandler.all_movies_)
+        ratings = User.select().where(User.archived == False)
+        for i in ratings:
+            vote_number = len(i.grades)
+            if vote_number < number_of_movies:
+                i.grades += "1" * (number_of_movies - vote_number)
+                i.save()
+
+    def handle_request(request):
+        ''' takes request from user, returns dict for adding_new_rating '''
+        ip = request.remote_addr
+        rated_ids = [i for i in request.form if i[:3] == "Mid"]
+        grades = [request.form[i] for i in rated_ids]
+        indexes = [int(i[3:]) for i in rated_ids]
+
+        return ip, indexes, grades
+
+    def add_new_rating(ip, indexes, grades):
+        ''' add retrieved data to database
+            returns new user object '''
+
+        # archive previous rating (if any)
+        selection = User.select().where(User.ip_address == ip)
+        selection = selection.where(User.archived == False)
+        # archive older rating logs (if available)
+        if len(selection) > 0:
+            for i in selection:
+                i.archived = True
+                i.save()
+
+        # add new rating log
+        new_user = User.create(ip_address=ip,
+                               grades="1" * len(DBHandler.all_movies_))
+
+        # edit grades
+        new_string = new_user.grades
+        for (i, v) in zip(indexes, grades):
+            new_string = new_string[:i] + str(v) + new_string[i + 1:]
+
+        new_user.grades = new_string
+        new_user.save()
+        return new_user
+
+    def query_ratings(indexes, current_user_id):
+        ''' query all User.grades for the [indexes] movies '''
+        selection = User.select().where(User.archived == False)
+
+        # grab what grades all users have given to these movies
+        grades = []
+        user_ids = []
+        for i in selection:
+            # ignore current user
+            if i.id == current_user_id:
+                continue
+
+            grade = [int(i.grades[index]) for index in indexes]
+            grades.append(grade)
+            user_ids.append(i.id)
+        return grades, user_ids
+
+    def get_closest_fit(grades, labels, my_grades):
+        ''' input:
+                grades is an array where each element is a string/array
+                containing all the grades a given user gave to the movies
+                being queried.
+
+                labels is an array where each element is the id of the user
+                who's grades are in the grades array
+
+                my_grades is a string/array containing the user's votes/grades
+                for the movies in question
+
+            ouput: id of the Users that most closely match my_grades '''
+
+        # fit the model
+        my_classifier = KNeighborsClassifier(n_neighbors=1)
+        my_classifier.fit(grades, labels)
+
+        # get selection
+        my_grades_int = [int(i) for i in my_grades]
+        selection = my_classifier.kneighbors([my_grades_int], 1, False)
+
+        # get id of selected users
+        closest_people = [labels[int(i)] for i in selection[0]]
+        return closest_people
+
+    def extract_recomendations(user_object, closest_people):
+        ''' input:
+                closest_people (array):
+                    array of users that have similar tastes
+                user_object (User instance):
+                    current user's instance
+
+            output:
+                array of Movie.id of recommended movies '''
+        selection = User.select().where(User.id << closest_people)
+        similar_grades = [i.grades for i in selection]
+        user_grades = user_object.grades
+        zips = [zip(user_grades, similar) for similar in similar_grades]
+
+        recommendations = []
+        for zipped in zips:
+            for (index, (user_grade, recommender_grade)) in enumerate(zipped):
+                if user_grade == "1" and recommender_grade == "2":
+                    recommendations.append(index + 1)
+
+        return recommendations
+
+    def sort_recomendations(array):
+        ''' sort recommendations by highest imdb grade '''
+        recommended = Counter(array).most_common()
+        sorted_recommended = [k for k, v in recommended if
+                              k in DBHandler.all_movies_ids_]
+
+        # when list ends, start recommending from highest unwatched movie
+        sorted_rest = []
+        for i in DBHandler.all_movies_:
+            movie_id = i.id
+            if movie_id not in array:
+                sorted_rest.append(movie_id)
+
+        # full list:
+        all_recomendations_ids = sorted_recommended + sorted_rest
+
+        return all_recomendations_ids
+
+    def load_all_movies():
+        ''' load all movies, in order not to query again '''
+        all_movies = Movie.select().where(Movie.archived == False)
+        ordered_movies = all_movies.order_by(Movie.grade.desc())
+
+        # all movies that are not archived, ordered by rating:
+        DBHandler.all_movies_ = ordered_movies
+
+        # and their ids
+        DBHandler.all_movies_ids_ = [i.id for i in DBHandler.all_movies_]
+
+    def retireve_movie_info(movie_ids):
+        # only search for id of non-archived movies
+        filtered_ids = [i for i in movie_ids if i in DBHandler.all_movies_ids_]
+        # find and sort movies
+        selection = Movie.select().where(Movie.id << filtered_ids)
+        sorted_selection = selection.order_by(Movie.grade.desc())
+        # return array, where i is a dict of the movie info
+        return [model_to_dict(i) for i in sorted_selection]
+
+    # TODO
+    # add possibilities to filter by categories
+
+
+def setup_db():
+    DBHandler.load_all_movies()
+    DBHandler.update_ratings_length()
+
+
+def initialize_db():
+    db.connect()
+    db.create_tables([User, Movie], safe=True)
---
+++ scrape imdb/scrapeTop250.py
@@ -0,0 +1,167 @@
+import bs4 as bs
+import requests
+import database_model as d
+from selenium import webdriver as w
+from time import sleep
+
+# start DB
+d.initialize_db()
+
+
+def download_and_save(url, target_filename=False):
+    ''' downloads and saves target url '''
+
+    # request page
+    pg = requests.get(url)
+    # confirm it was properly saved
+    pg.raise_for_status()
+    # save it as a file on current folder
+    if target_filename:
+        with open(target_filename, mode="wb") as f:
+            for chunk in pg.iter_content(100000):
+                f.write(chunk)
+
+    soup = bs.BeautifulSoup(pg.content, "html.parser")
+
+    return soup
+
+
+def scrape_top250(pg):
+    ''' opens saved html file and returns dict movienames:info '''
+
+    # select table with all movies
+    table = pg.find("tbody", class_="lister-list")
+    # get all rows
+    rows = table.find_all("tr")
+    # get td for the names
+    titles = [i.find("td", class_="titleColumn") for i in rows]
+    # get names
+    stripped_names = [i.text.split("\n")[2].strip() for i in titles]
+    # get links
+    hrefs = [i.find("a")["href"] for i in titles]
+    # get ratings
+    ratingsCol = [i.find("td", class_="ratingColumn imdbRating") for i in rows]
+    ratings = [i.text.replace("\n", "") for i in ratingsCol]
+    # get sinopses and posters
+    sinopses = []
+    posters = []
+    counter = len(hrefs)
+    for i in hrefs:
+        print("Downloading info from:", i)
+        print(counter, "movies left")
+        counter -= 1
+        sinopse, poster = scrape_movie_info(i)
+        sinopses.append(sinopse)
+        posters.append(poster)
+
+    # reasign posters to high definition version
+    posters = get_high_def_posters(posters)
+
+    # assert data integrity
+    data_size = len(hrefs)
+    assert all(map(lambda x: len(x) == data_size, [sinopses, posters,
+               stripped_names, ratings])), "DATA SIZES ARE NOT EQUAL"
+    # return zip object
+    return zip(stripped_names, hrefs, ratings, sinopses, posters)
+
+
+def get_high_def_posters(array):
+    # higher def links
+    posters = []
+    # use Selenium, because pages are rendered by JS
+    browser = w.Chrome("C:\\Users\\Kepelrs\\Dropbox\\DAVI\\SCRIPTS\\chromedriver_win32\\chromedriver.exe")
+    browser.get("http://www.google.com")
+    sleep(3)
+    # iterate over all url's in the array
+    for link in array:
+        url = "http://www.imdb.com" + link
+        # get page and sleep in order for JS to load
+        browser.get(url)
+        sleep(3)
+        # get HTML of rendered page
+        html = browser.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+        pg = bs.BeautifulSoup(html, "html.parser")
+        # get img elements
+        matches = pg.find_all(class_="pswp__img")
+        # get only the ones that have an "alt" attribute
+        filtered_matches = []
+        for i in matches:
+            try:
+                i["alt"]
+            except:
+                continue
+            filtered_matches.append(i)
+        # select only the main img element if there are multiple matches
+        try:
+            poster = filtered_matches[1]["src"]
+        except:
+            poster = filtered_matches[0]["src"]
+        # store in posters array
+        posters.append(poster)
+
+    # close browser
+    browser.quit()
+
+    return posters
+
+
+def add_movies_to_db(zip_obj):
+    for i in zip_obj:
+        movie_status, movie = check_if_movie_up_to_date(i)
+        if movie_status == "Not in database":
+            d.Movie.create(name=i[0],
+                           grade=i[2],
+                           sinopse=i[3],
+                           imdb_link=i[1],
+                           poster_link=i[4],
+                           )
+        elif movie_status == "Not up to date":
+            movie.grade = i[2]
+            movie.sinopse = i[3]
+            movie.imdb_link = i[1]
+            movie.poster_link = i[4]
+            movie.save()
+        print(i[0], movie_status)
+
+
+def check_if_movie_up_to_date(movie_info_array):
+    # reasing (for semantics only)
+    i = movie_info_array
+    # select movies a given name
+    selection = d.Movie.select().where(d.Movie.name == i[0])
+    movies = selection.where(d.Movie.archived == False)
+    # sanity check number of matches
+    number_of_matches = len(movies)
+    if number_of_matches > 1:
+        raise IndexError("Too many movies matched for", i[0])
+    elif number_of_matches == 1:
+        movie = movies[0]
+    else:
+        movie = False
+
+    # check status of information for a given movie
+    if movie:
+        if (movie.grade == float(i[2]) and movie.sinopse == i[3] and
+            movie.imdb_link == i[1] and movie.poster_link == i[4]):
+            return "Up to date", movie
+        return "Not up to date", movie
+    return "Not in database", False
+
+
+def scrape_movie_info(link):
+    url = "http://www.imdb.com" + link
+    pg = download_and_save(url)
+
+    # get sinopse:
+    sinopse = pg.find("div", class_="summary_text").text.strip()
+    # get poster
+    poster = pg.find("div", class_="poster").find("a")["href"]
+    return sinopse, poster
+
+
+top250 = download_and_save("http://www.imdb.com/chart/top", "CurrentPage.html")
+all_movies = scrape_top250(top250)
+
+add_movies_to_db(all_movies)
+
+print("SCRAPING DONE")
